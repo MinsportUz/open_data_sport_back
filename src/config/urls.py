57b22015e2728331530b4563596144037b63aad6
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-01-09 20:16:42 +0500

    change code

--- src/config/urls.py
+++ src/config/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from .api import urlpatterns as api_urlpatterns
 from django.conf.urls.i18n import i18n_patterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,6 @@ urlpatterns = [
 urlpatterns += i18n_patterns(
     path('', include(api_urlpatterns)),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
