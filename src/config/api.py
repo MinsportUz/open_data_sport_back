6aa52825b1177dbb2e8fd0387c176d7601281e0f
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-01-09 19:56:55 +0500

    .

--- src/config/api.py
+++ src/config/api.py
@@ -1,9 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from app import views as app_views
 from sport import views as sport_views
 from utils import views as utils_views
 
+api = routers.DefaultRouter()
+
+# utils urls
+api.register(r'state', utils_views.StateView, basename='state')
+
 urlpatterns = [
+    path('', include(api.urls)),
     path('lockout/', app_views.lockout_view, name='lockout_url'),
 ]
