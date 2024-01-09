eade1b64d9cb0cdaf217b92156836901007fb10d
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-01-09 20:23:33 +0500

    change code

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SportData
+
+# admin.site.register(SportData)
+
+admin.site.site_header = 'Data Sport Admin panel'
+admin.site.site_title = 'Data Sport  Admin panel'
+admin.site.index_title = 'Data Sport  Admin panel'
+
+
+@admin.register(SportData)
+class SportDataAdmin(admin.ModelAdmin):
+    list_filter = ('title', 'state', 'sport_type', 'created_at')
+    search_fields = ('title', 'attr')
+    list_per_page = 20
--- src/config/urls.py
+++ src/config/urls.py
@@ -21,7 +21,10 @@ from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .yasg import schema_view
+
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
 ]
 
