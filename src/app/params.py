e7e05b77ad0073a89dc2e598e57fe04d6f698229
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-01-10 17:28:20 +0500

    change ...

--- src/app/params.py
+++ src/app/params.py
@@ -2,6 +2,6 @@ from drf_yasg import openapi
 
 
 params = [
-    openapi.Parameter('title', openapi.IN_QUERY, description="title", type=openapi.TYPE_STRING, required=True),
-    openapi.Parameter('author', openapi.IN_QUERY, description="author", type=openapi.TYPE_STRING, required=True),
+    openapi.Parameter('title', openapi.IN_QUERY, description="title", type=openapi.TYPE_STRING),
+    openapi.Parameter('author', openapi.IN_QUERY, description="author", type=openapi.TYPE_STRING),
 ]
