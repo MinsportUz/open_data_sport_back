e9203066438cabfbd00461bac01413ae647c4d72
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-01-18 09:31:47 +0500

    add test to project

--- src/app/tests.py
+++ src/app/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from .models import SportData, LegislativeDocument, About, Footer
 # Create your tests here.
+
+# Path: src/app/tests.py
+# Compare this snippet from src/social/admin.py:
+
+
+class TestSocial(TestCase):
+    def test_social(self):
+        self.assertEqual(1, 1)
+# Compare this snippet from src/social/models.py:
+
+
+class SportDataTest(TestCase):
+    def test_sport_data(self):
+        self.assertEqual(1, 1)
+
