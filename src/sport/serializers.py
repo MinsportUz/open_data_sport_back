facb696c2224ffeba806e6ef24590f487084fbd1
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-01-18 14:58:08 +0500

    change sport_type serializers.py

--- src/sport/serializers.py
+++ src/sport/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
+from django.db.models import Sum
 
 from .models import SportType
+
+from app.models import SportData
+from utils.models import State
 
 
 class SportTypeSerializers(serializers.ModelSerializer):
     class Meta:
         model = SportType
-        fields = ('id', 'title', 'attr', 'icon', 'state')
+        fields = ('id', 'title', 'attr', 'icon')
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['count'] = SportData.objects.filter(sport_type=instance, state=State.objects.first()).count()
+        representation['file_views'] = SportData.objects.filter(sport_type=instance, state=State.objects.first()).aggregate(
+            views=Sum('views'))['views'] or 0
+        return representation
