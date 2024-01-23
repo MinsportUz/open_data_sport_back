d24ff0d5c9600e17de95ab7ed2434f6bedff53f2
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-01-23 12:28:39 +0500

    change configuration

--- src/config/yasg.py
+++ src/config/yasg.py
@@ -3,12 +3,13 @@ from drf_yasg import openapi
 from rest_framework import permissions
 from rest_framework.authentication import TokenAuthentication  # Import authentication classes needed
 from rest_framework_simplejwt.authentication import JWTAuthentication  # Import JWT Authentication if you're using it
+from django.conf import settings
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="data.sport Uz Api",
+        title="darslik.sport.uz Api",
         default_version='v1',
-        description="data.sport Uz Api documentations",
+        description="darslik.sport.uz Api documentations",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="haydarovakbar640@gmail.com"),
         license=openapi.License(name="BSD License"),
@@ -16,6 +17,6 @@ schema_view = get_schema_view(
     public=True,
     permission_classes=(permissions.AllowAny,),
     # patterns=[path('api/', include('myapi.urls')), ],
-    # url=settings.HOST,
+    url=settings.HOST,
     authentication_classes=(TokenAuthentication, JWTAuthentication),
 )
